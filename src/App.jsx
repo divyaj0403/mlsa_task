981bc02f64c556683a66e0f0ff74eef89a620200
Divya <divyajunghare04@gmail.com>
2024-01-07 12:53:38 +0530

    add changes

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Divya Junghare",
+  title: "Web Developer & Data Analyst",
+  email: "Divya.Junghare@studentambassadors.com",
+  gitHub: "divyaj0403",
+  instagram: "",
+  linkedIn: "divyaj0403/",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -19,9 +19,9 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/design-desk.jpeg";
+import image from "../images/first.jpg";
 
-const imageAltText = "desktop with books and laptop";
+const imageAltText = "street image";
 
 /**
  * Project list
@@ -31,28 +31,28 @@ const imageAltText = "desktop with books and laptop";
  */
 const projectList = [
   {
-    title: "10 Things To Know About Azure Static Web Apps 🎉",
+    title: "FRT Project",
     description:
-      "Collaboration to create a beginner friendly article to help explain Azure Static Web Apps and tooling to get started.",
-    url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
+       "my projects.",
+    url: " https://wonderful-island-01d7a2410.3.azurestaticapps.net",
   },
   {
-    title: "Web Development for Beginners",
+    title: "Hacktober first",
     description:
-      "Contributed sketch note imagery to accompany each lesson. These help provide visual representation of what is being taught.",
-    url: "https://github.com/microsoft/web-dev-for-beginners",
+      "Hacktoberfest Contribution.",
+    url: "https://github.com/AkankshaAI/Hacktoberfest2023-Beginners-New.git",
   },
   {
-    title: "My Resume Site",
+    title: "Medisphere",
     description:
-      "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
-    url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
+      "Health insights website",
+    url: "https://divyaj0403.github.io/Medisphere/",
   },
   {
-    title: "GitHub Codespaces and github.dev",
+    title: "My portfolio website",
     description:
-      "Video interview to explain when to use GitHub.dev versus GitHub Codespaces, and how best to use each tool.",
-    url: "https://www.youtube.com/watch?v=c3hHhRME_XI",
+      "Portfolio website",
+    url: "divyaj0403.github.io/portfolio.github.io/",
   },
 ];
 
